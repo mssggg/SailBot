7cecf2bdc162224fa5cb7c06a7ea315fc9eebe8b
Yan Sun <e2h0b@bowen.ugrad.cs.ubc.ca>
2016-02-07 13:21:03 -0800

    modified constructor

--- SphericalDistanceCalculator.cpp
+++ SphericalDistanceCalculator.cpp
@@ -1,23 +1,38 @@
+/*
+ * SphericalDistanceCalculator.cpp
+
+ */
+
 #include "SphericalDistanceCalculator.h"
-#include <iostream>
-#include"GPSCoordinate.h"
-using namespace std;
 
-SphericalDisanceCalculator::SphericalDistanceCalculator(double r) {}
- double SphericalDisanceCalculator::calculateDistance( GPSCoordinate *coord1,  GPSCoordinate *coord2) {
-	 double lat1r, lon1r, lat2r, lon2r, u, v;
-	 double lat1 = coord1->lat;
-	 double lon1 = coord1->lon;
-	 double lat2 = coord2->lat;
-	 double lon2 = coord2->lon;
+#include <cmath>
+
+const double pi = 3.14159265358979323846;
+
+SphericalDistanceCalculator::SphericalDistanceCalculator(double radius) {
+	// TODO Auto-generated constructor stub
+	radius_ = radius;
+}
+
+SphericalDistanceCalculator::~SphericalDistanceCalculator() {
+	// TODO Auto-generated destructor stub
+}
 
 
-	 lat1r = deg2rad(lat1* pi / 180);
-	 lon1r = deg2rad(lon1* pi / 180);
-	 lat2r = deg2rad(lat2* pi / 180);
-	 lon2r = deg2rad(lon2* pi / 180);
-	 u = sin((lat2r - lat1r) / 2);
-	 v = sin((lon2r - lon1r) / 2);
-	 return 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r)* v * v));
-	}
+double SphericalDistanceCalculator::calculateDistance(GPSCoordinate *coord1, GPSCoordinate *coord2)
+{
+	double lat1r, lon1r, lat2r, lon2r, u, v;
+	double lat1 = coord1->lat;
+	double lon1 = coord1->lon;
+	double lat2 = coord2->lat;
+	double lon2 = coord2->lon;
 
+
+	lat1r = deg2rad(lat1);
+	lon1r = deg2rad(lon1);
+	lat2r = deg2rad(lat2);
+	lon2r = deg2rad(lon2);
+	u = sin((lat2r - lat1r) / 2);
+	v = sin((lon2r - lon1r) / 2);
+	return 2.0 * radius_ * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
+}
